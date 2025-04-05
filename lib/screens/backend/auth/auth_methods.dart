7b90b4ba51344e8ef90a89d49a92c4b241b96f6f
LokeshSuryaPrakashK <lokeshsuryaprakashk@gmail.com>
2025-04-05 22:05:46 +0530

    Refactor AuthMethods class to use User model and streamline signIn, signUp, and signOut methods

--- lib/screens/backend/auth/auth_methods.dart
+++ lib/screens/backend/auth/auth_methods.dart
@@ -1,4 +1,3 @@
-import 'package:whatsapp_clone/screens/backend/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthMethods {
@@ -6,16 +5,40 @@ class AuthMethods {
   // For example, signIn, signUp, signOut, etc.
   // You can use Firebase Auth or any other authentication service you prefer.
   // Example:
-  Future<UserModel?> signIn(String email, String password) async {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
     try {
-      UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
+      return userCredential.user;
     } catch (e) {
       print(e);
       return null;
+    }
+  }
+
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      print(e);
     }
   }
 }
